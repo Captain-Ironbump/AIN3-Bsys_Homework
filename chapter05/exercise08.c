ba1bf58a5a1a7be23a0682dfecc10d5fd39a5caa
Captain-Ironbump <lukas.linde.99@web.de>
2022-10-10 14:54:15 +0200

    more things to work on lol

--- chapter05/exercise08.c
+++ chapter05/exercise08.c
@@ -6,42 +6,53 @@
 #include <sys/wait.h>
 
 int main(int argc, char *argv[]) {
-    int rc = fork();
     char buf;
     int pipefd[2];
+
+    int rc2 = fork();
+
+    int rc1 = fork();
 
     if (pipe(pipefd) == -1) {
         perror("pipe");
         exit(EXIT_FAILURE);
     }
 
-    if (rc < 0)
+    if (rc1 < 0 && rc2 < 0)
     {
         fprintf(stderr, "fork failed\n");
         exit(1);
-    } else if (rc == 0) {
+    } else if (rc1 == 0 && rc2 > 0) {
         // child process 1
+        printf("Child 1\n");
         close(pipefd[0]);   // close unused read end
-        write(pipefd[1], argv[1], strlen(argv[1]));
+        printf("Argv: %s\n", argv[1]);
+        if (write(pipefd[1], argv[1], strlen(argv[1])) == -1) {
+            perror("Write\n");
+            exit(EXIT_FAILURE);
+        }
+        printf("Write\n");
         close(pipefd[1]);          // Reader will see EOF
-        wait(NULL);                // Wait
+        //wait(NULL);                // Wait
         exit(EXIT_SUCCESS);
 
-    } else if (rc == 1) {
+    } else if (rc1 > 0 && rc2 == 0) {
         // child process 2
+        waitpid(rc1, NULL, 0);
+
         close(pipefd[1]);   // close unused write end
+        printf("Read: %ld\n", read(pipefd[0], &buf, 1));
         while (read(pipefd[0], &buf, 1) > 0)
             write(STDOUT_FILENO, &buf, 1);
         write(STDOUT_FILENO, "\n", 1);
         close(pipefd[0]);
         _exit(EXIT_SUCCESS);
-        
 
-    } else {
+
+    } else if (rc1 > 0 && rc2 > 0) {
         // parent
-        printf("Hello\n");
-        printf("%d\n", pipefd[0]);
+        printf("Hello, I am parent\n");
+        //printf("%d\n", pipefd[0]);
     }
     return 0;
 }
-    
